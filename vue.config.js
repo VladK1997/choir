b1d6f14fafb51aafdbb031978b3bf64028d309de
vladk <vladkukhlii@gmail.com>
2022-10-05 03:57:24 +0300

    init

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ const path = require("path");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "",
+  publicPath: "/choir",
   outputDir: path.resolve(__dirname, "./docs"),
 })
